e6109c676db59fd88a2de2e8f11cdb081513c75e
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-03 10:39:44 -0400

    did basic ask anwer, no enhancecments

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,6 +1,13 @@
 class CoachingController < ApplicationController
   def answer
     @query = params[:query]
+    if @query.downcase == "i am going to work right now!"
+      @answer = ""
+    elsif @query.end_with?("?") == true
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @query.end_with?("?") == false
+      @answer = "I don\'t care, get dressed and go to work!"
+    end
   end
 
   def ask
